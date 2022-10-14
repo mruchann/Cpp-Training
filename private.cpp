274114c99cb5d25f2b0aff4d16dad8ed473252bd
Mehmet Rüçhan Yavuzdemir <88430247+mruchann@users.noreply.github.com>
2022-10-15 01:50:58 +0300

    Create private.cpp

---
+++ private.cpp
@@ -0,0 +1,7 @@
+// private functions are useful when we want to do something within the class but we forbid the usage out of the class.
+
+// helper functions within the class are usually put in private.
+
+// everything is visibile within a class, you can access private sections WITHIN a class. 
+
+// If Foo() is private function, Entity::Foo() is not available outside of the class, we cannot use that function. Only other functions can use it.
