b1177a9ed4aac9b81bc05ad644eb075c68a37501
ruchan <ruchan.yavuzdemir@icloud.com>
2022-10-29 21:14:48 +0300

    Delete unnecessary files

--- deneme.cpp
+++ deneme.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+template <class T>
+class Deneme 
+{
+public:
+	Deneme(T x) : data(x){}  
+	T data;
+};
+
+
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	Deneme<int> *NewItem = new Deneme<int>();
+
+	std::cout << NewItem->data << std::endl;
+
 	return 0;
 }
